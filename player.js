d13528e4b0e3e42b938619f48cb12ceb2801d9cb
wendellmoraisz <wendellmorais3319@gmail.com>
2022-02-15 15:30:38 -0300

    playlist

--- player.js
+++ player.js
@@ -1,51 +1,62 @@
 const pause = document.querySelector('#pause')
 const play = document.querySelector('#play')
 const music = document.querySelector('#music')
-const barra = document.querySelector('#bar')
-const proximo = document.querySelector('#next')
+const bar = document.querySelector('#bar')
+const next = document.querySelector('#next')
+const back = document.getElementById('back')
 
+back.onclick = voltar
 play.onclick = comecar
 pause.onclick = pausar
-let indexMusica = 0
+next.onclick = function(){
+ nextMusic(musics[getMusic()])
+}
+    
+let indexMusic = 0
 
-const musicas = [{
-    titulo: "Hollywood's Bleeding",
-    artista: 'Post Malone',
-    capa: 'img/postMalone.jpg',
+const musics = [{
+    title: "Hollywood's Bleeding",
+    artist: 'Post Malone',
+    albumCover: 'img/postMalone.jpg',
     audio: 'audio/malone.mp3'
 }, {
-    titulo: "I Don't Wanna Live Forever",
-    artista: 'Taylor Swift ft. ZAYN',
-    capa: 'img/taylor_swift.jpg',
+    title: "I Don't Wanna Live Forever",
+    artist: 'Taylor Swift ft. ZAYN',
+    albumCover: 'img/taylor_swift.jpg',
     audio: 'audio/taylor.mp3'
 }, {
-    titulo: 'Somewhere I Belong',
-    artista: 'Linkin Park',
-    capa: 'img/meteora.jpg',
+    title: 'Somewhere I Belong',
+    artist: 'Linkin Park',
+    albumCover: 'img/meteora.jpg',
     audio: 'audio/somewhere_i_belong.mp3'
 }, {
-    titulo: 'Harvester Of Sorrow',
-    artista: 'Metallica',
-    capa: 'img/and_justice_for_all.webp',
+    title: 'Harvester Of Sorrow',
+    artist: 'Metallica',
+    albumCover: 'img/and_justice_for_all.webp',
     audio: 'audio/harvester_of_sorrow.mp3'
 }]
 
-function getMusica() {
-    indexMusica == musicas.length - 1 ? indexMusica = 0 : indexMusica++
-    return indexMusica
+function getMusic() {
+    indexMusic == musics.length - 1 ? indexMusic = 0 : indexMusic++
+    return indexMusic
 }
 
-function passarMusica(obj) {
-    document.querySelector('.title').innerHTML = obj.titulo
-    document.querySelector('.artist').innerHTML = obj.artista
-    document.querySelector('#capa').src = obj.capa
+function nextMusic(obj) {
+    document.querySelector('.title').innerHTML = obj.title
+    document.querySelector('.artist').innerHTML = obj.artist
+    document.querySelector('#albumCover').src = obj.albumCover
     document.querySelector('#music').src = obj.audio
+
+    musicsFromPlaylist.forEach(e => e.classList.remove('selected'))
+    musicsFromPlaylist[indexMusic].classList.add('selected')
 }
 
 function comecar() {
     music.play()
     play.style.display = 'none'
     pause.style.display = 'inline'
+
+    musicsFromPlaylist[indexMusic].classList.add('selected')
 }
 
 function pausar() {
@@ -57,33 +68,35 @@ function pausar() {
 function voltar() {
     if(music.currentTime > 1.5){
         music.currentTime = 0
-    } else if (indexMusica == 0){
-        indexMusica = musicas.length - 1
-        passarMusica(musicas[indexMusica])
+    } else if (indexMusic == 0){
+        indexMusic = musics.length - 1
+        nextMusic(musics[indexMusic])
     } else {
-        indexMusica -= 1
-        passarMusica(musicas[indexMusica])
+        indexMusic -= 1
+        nextMusic(musics[indexMusic])
     }
 }
 
 function progress() {
     const percentual = (music.currentTime / music.duration) * 100
-    barra.style.width = `${percentual}%`
+    bar.style.width = `${percentual}%`
 
     if (music.currentTime == music.duration) {
-        passarMusica(musicas[getMusica()])
+        nextMusic(musics[getMusic()])
     }
 }
 
 window.setInterval(progress, 1)
 
 const playlist = document.querySelector('.playlist > ul')
-musicas.forEach(e => playlist.innerHTML += `<li><a href="#">${e.titulo} - ${e.artista}</a></li>`)
+musics.forEach(e => playlist.innerHTML += `<li><a href="#">${e.title} - ${e.artist}</a></li>`)
 
 const musicsFromPlaylist = document.querySelectorAll('.playlist > ul > li')
 
 musicsFromPlaylist.forEach((e,i) => {
     e.onclick = function(){
-        passarMusica(musicas[i])
+        nextMusic(musics[i])
+        musicsFromPlaylist.forEach(e => e.classList.remove('selected'))
+        e.classList.add('selected')
 }
 })
